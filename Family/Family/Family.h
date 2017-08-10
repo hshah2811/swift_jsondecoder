bd5987f01ae2db257af9ccd9f4f0d9274381aafe
harshit <harshit.shah@c-sam.com>
2017-08-10 14:33:04 +0530

    Initial commit. Family framework added.

---
+++ Family/Family/Family.h
@@ -0,0 +1,19 @@
+//
+//  Family.h
+//  Family
+//
+//  Created by Shah, Harshit on 8/10/17.
+//  Copyright © 2017 Harshit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Family.
+FOUNDATION_EXPORT double FamilyVersionNumber;
+
+//! Project version string for Family.
+FOUNDATION_EXPORT const unsigned char FamilyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Family/PublicHeader.h>
+
+
